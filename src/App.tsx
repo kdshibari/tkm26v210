f68089978d007e8f318e4b425a28fb7c7e98209e
tkm <thekinkymap@gmail.com>
2026-03-03 21:16:38 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -5,23 +6,48 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { IdentityModal } from "./IdentityModal";
+import { defaultIdentity } from "./IdentityData";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  const [isIdentityModalOpen, setIsIdentityModalOpen] = useState(false);
+  const [meIdentity, setMeIdentity] = useState(defaultIdentity);
+  const [partnerIdentity, setPartnerIdentity] = useState(defaultIdentity);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route 
+              path="/" 
+              element={
+                <Index 
+                  setIsIdentityModalOpen={setIsIdentityModalOpen}
+                  meIdentity={meIdentity}
+                  partnerIdentity={partnerIdentity}
+                />
+              } 
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        
+        <IdentityModal
+          isOpen={isIdentityModalOpen}
+          onClose={() => setIsIdentityModalOpen(false)}
+          meIdentity={meIdentity}
+          setMeIdentity={setMeIdentity}
+          partnerIdentity={partnerIdentity}
+          setPartnerIdentity={setPartnerIdentity}
+        />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
