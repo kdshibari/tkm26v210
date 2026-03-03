50e431d1c40889c0b56764ca190115fa99ba5737
tkm <thekinkymap@gmail.com>
2026-03-03 21:15:19 +0700

    Update IdentityData.ts

--- src/IdentityData.ts
+++ src/IdentityData.ts
@@ -1,1 +1,26 @@
+export const pronounsList = [
+  "He/Him", "She/Her", "They/Them", "Ze/Zir", "Any", "Ask Me", "Prefer not to say"
+];
+export const genderList = [
+  "Woman", "Man", "Nonbinary", "Genderfluid", "Agender", "Bigender", "Questioning", "Prefer not to say"
+];
+export const orientationList = [
+  "Heterosexual", "Homosexual", "Bisexual", "Pansexual", "Asexual", "Demisexual", "Queer", "Prefer not to say"
+];
+export const relationshipList = [
+  "Monogamous", "Polyamorous", "Open", "Ethical Nonmonogamy", "Relationship Anarchy", "Prefer not to say"
+];
 
+export interface IdentityState {
+  pronouns: string;
+  gender: string;
+  orientation: string;
+  relationship: string;
+}
+
+export const defaultIdentity: IdentityState = {
+  pronouns: "",
+  gender: "",
+  orientation: "",
+  relationship: ""
+};
