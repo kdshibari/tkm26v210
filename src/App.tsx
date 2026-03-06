65921b199d4d76a8f40432546d5ba4f2f4ffb5b4
tkm <thekinkymap@gmail.com>
2026-03-06 21:24:44 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -6,16 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { IdentityModal } from "./IdentityModal";
-import { defaultIdentity } from "./IdentityData";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  const [isIdentityModalOpen, setIsIdentityModalOpen] = useState(false);
-  const [meIdentity, setMeIdentity] = useState(defaultIdentity);
-  const [partnerIdentity, setPartnerIdentity] = useState(defaultIdentity);
-
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
@@ -23,30 +16,10 @@ const App = () => {
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route 
-              path="/" 
-              element={
-                <Index 
-                  setIsIdentityModalOpen={setIsIdentityModalOpen}
-                  meIdentity={meIdentity}
-                  setMeIdentity={setMeIdentity}
-                  partnerIdentity={partnerIdentity}
-                  setPartnerIdentity={setPartnerIdentity}
-                />
-              } 
-            />
+            <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-        
-        <IdentityModal
-          isOpen={isIdentityModalOpen}
-          onClose={() => setIsIdentityModalOpen(false)}
-          meIdentity={meIdentity}
-          setMeIdentity={setMeIdentity}
-          partnerIdentity={partnerIdentity}
-          setPartnerIdentity={setPartnerIdentity}
-        />
       </TooltipProvider>
     </QueryClientProvider>
   );
